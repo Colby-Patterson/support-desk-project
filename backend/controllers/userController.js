acc1efe4756782e20e9ab136d9d5fc13508a1ee9
Colby Patterson <cpatt@byui.edu>
2022-08-25 13:27:13 -0600

    Error handler and async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,19 @@
-const registerUser = (req, res) => {
-  res.send("Register route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+// @route /api/users
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+  res.send("Register route");
+});
+
+// @route /api/users/login
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login route");
-};
+});
 
 module.exports = {
   registerUser,
