be89e6e7f0318acb0d27f5cb443844ecbf66d00c
Colby Patterson <cpatt@byui.edu>
2022-08-29 16:56:11 -0600

    Get and create tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -4,11 +4,43 @@ const User = require("../models/userModel");
 const Ticket = require("../models/ticketModel");
 
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get tickets" });
+  // Get user using ID in JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: "Create ticket" });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("Please add a product and description");
+  }
+
+  // Get user using ID in JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "New",
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = {
